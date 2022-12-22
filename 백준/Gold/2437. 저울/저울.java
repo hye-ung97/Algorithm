5a7097000111ae52e5fcc27856b633de0805d531
hye-ung97 <117243197+hye-ung97@users.noreply.github.com>
2022-12-22 20:53:04 +0900

    [Gold II] Title: 저울, Time: 276 ms, Memory: 19908 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[]arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(sum + 1 < arr[i]){
+                break;
+            }
+            sum = sum + arr[i];
+        }
+
+        System.out.println(sum + 1);
+    }
+}
