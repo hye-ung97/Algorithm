82ac60f55cd53ab9bde72d66ecb51f678924733c
hye-ung97 <117243197+hye-ung97@users.noreply.github.com>
2023-01-30 09:54:29 +0900

    [Gold V] Title: 꿀 따기, Time: 224 ms, Memory: 19460 KB -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.java
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -1,63 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] Honey;
+    public static long[] Honey;
     public static int N;
-    public static int CNT;
+    public static long CNT;
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        Honey = new int[N];
+        Honey = new long[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int a = Integer.parseInt(st.nextToken());
+            long a = Long.parseLong(st.nextToken());
             Honey[i] = a;
         }
 
         CNT = 0;
 
         //벌 왼쪽 고정, 꿀통 오른쪽 고정
+        long fixBeeSum = Arrays.stream(Honey).sum() - Honey[0];
+        long moveBeeSum = fixBeeSum;
 
         for (int i = 1; i <= Honey.length - 2 ; i++) {
-            int sum = 0;
-            for (int j = 1; j <= Honey.length - 1; j++) {
-                if(j == i){
-                    continue;
-                }
-                sum += Honey[j];
-            }
-            for (int j = i + 1; j <= Honey.length - 1 ; j++) {
-                sum += Honey[j];
-            }
+            long sum = fixBeeSum - Honey[i];
+            moveBeeSum = moveBeeSum - Honey[i];
+            sum += moveBeeSum;
             CNT = Math.max(sum, CNT);
         }
 
 
         //벌 오른쪽 고정, 꿀통 왼쪽 고정
+        fixBeeSum = fixBeeSum + Honey[0] - Honey[Honey.length - 1];
+        moveBeeSum = fixBeeSum;
         for (int i = Honey.length - 2; i >= 0 ; i--) {
-            int sum = 0;
-            for (int j = Honey.length - 2; j >= 0 ; j--) {
-                if(j == i){
-                    continue;
-                }
-                sum += Honey[j];
-            }
-            for (int j = i - 1; j >= 0; j--) {
-                sum += Honey[j];
-            }
+            long sum = fixBeeSum - Honey[i];
+            moveBeeSum = moveBeeSum - Honey[i];
+            sum += moveBeeSum;
             CNT = Math.max(sum, CNT);
         }
 
 
         //벌 양쪽 고정, 꿀통 움직임
         for (int i = 1; i <= Honey.length - 2; i++) {
-            int sum = 0;
+            long sum = 0;
             //왼쪽벌
             for (int j = 1; j <= i; j++) {
                 sum += Honey[j];
