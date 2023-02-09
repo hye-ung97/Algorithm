4ab989c92fc3f477f1703932f15c10a7b8d844d3
hye-ung97 <117243197+hye-ung97@users.noreply.github.com>
2023-02-10 08:38:39 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 25.90 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,68 +1,97 @@
-import java.util.*;
- 
-class Solution {
-    public String[] solution(String[] files) {
-        String[] answer;
-        ArrayList<File> list = new ArrayList<>();
-        
-        for(int i=0; i<files.length; i++){
-            list.add(new File(files[i]));    
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+
+    public static class file implements Comparable<file>{
+        int idx;
+        String head;
+        String number;
+        String footer;
+
+        public file(int idx, String head, String number, String footer) {
+            this.idx = idx;
+            this.head = head;
+            this.number = number;
+            this.footer = footer;
         }
-        answer = new String[list.size()];
-        
-        Collections.sort(list);    // 정렬
-        
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i).head + list.get(i).number + list.get(i).tail;
-        }        
-                
+
+        @Override
+        public int compareTo(file o) {
+            String head1 = this.head.toLowerCase();
+            String head2 = o.head.toLowerCase();
+
+            int number1 = Integer.parseInt(this.number);
+            int number2 = Integer.parseInt(o.number);
+
+            if(head1.equals(head2)){
+                if(number1 == number2){
+                    return this.idx - o.idx;
+                }
+                else {
+                    return number1 - number2;
+                }
+            }
+            else{
+                return head1.compareTo(head2);
+            }
+        }
+    }
+
+    public static file parsing (String str, int n){
+        String head = "";
+        String number = "";
+        String footer = "";
+
+        int idx = 0;
+
+        while (true){
+            if(str.charAt(idx) >= '0' && str.charAt(idx) <= '9'){
+                break;
+            }
+            idx++;
+        }
+
+        head = str.substring(0, idx);
+
+        int pre = idx;
+
+        while (true){
+            idx++;
+            if(idx >= str.length() || str.charAt(idx) < '0' || str.charAt(idx) > '9'){
+                break;
+            }
+        }
+
+        number = str.substring(pre, idx);
+
+        if(idx < str.length() - 1){
+            footer = str.substring(idx);
+        }
+
+        return new file(n, head, number, footer);
+    }
+
+    public static String combine (file f){
+        return f.head + f.number + f.footer;
+    }
+
+    public static String[] solution(String[] files) {
+        String[] answer = new String[files.length];
+        ArrayList<file> newFile = new ArrayList<>();
+
+        for (int i = 0; i < files.length; i++) {
+            file temp = parsing(files[i], i);
+            newFile.add(temp);
+        }
+
+        Collections.sort(newFile);
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = combine(newFile.get(i));
+        }
+
         return answer;
     }
-    
-    static class File implements Comparable<File>{
-        int num;    // 들어온 순서
-        String head="";
-        String number="";
-        String tail="";
-        
-        /* 생성자-parsing */
-        File(String str){
-            int idx=0;
-            while(true){    // 숫자를 만나면 break
-                if(str.charAt(idx)>='0' && str.charAt(idx) <='9') break;
-                idx++;
-            }
-            this.head = str.substring(0,idx);
-            
-            String tmp = "";
-            while(true){
-                tmp += str.charAt(idx);
-                idx++;
-                if(idx >= str.length() || str.charAt(idx) < '0' || str.charAt(idx) >'9') break;
-            }
-            this.number = tmp;
-            
-            if(idx < str.length()){
-                this.tail = str.substring(idx);
-            }
-            
-        }
-        
-        @Override
-        public int compareTo(File o){
-            /* head 소문자로 변경 */
-            String str1 = this.head.toLowerCase();
-            String str2 = o.head.toLowerCase();
-            if(str1.equals(str2)) {    // head가 같다면
-                int num1 = Integer.parseInt(this.number);
-                int num2 = Integer.parseInt(o.number);
-                if(num1 == num2){    // number가 같다면
-                    return this.num - o.num; // 들어온 순서대로
-                }
-                return num1-num2;    // number 순서대로
-            }
-            
-            return str1.compareTo(str2);    // head 순서대로
-        }
-    }
+
 }
