93b3de5cf7cbeae1845b5dbb7a88fa2b8e4131aa
hye-ung97 <117243197+hye-ung97@users.noreply.github.com>
2023-06-12 14:33:25 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.17 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,46 +1,14 @@
-import java.util.*;
-
 class Solution {
-    public static String Change(int num, int n){
-        String temp = Integer.toString(num, 2);
-        
-        while(temp.length() < n){
-            temp = "0" + temp;
-        }
-        return temp;
-        
-    }
-    
-    public String decode(String a, String b, int n){
-        StringBuffer sb = new StringBuffer();
-        
-        for(int i = 0; i < n; i++){
-            if(a.charAt(i) == '1' || b.charAt(i) == '1'){
-                sb.append("#");
-            }
-            else{
-                sb.append(" ");
-            }
-        }
-        
-        return sb.toString();
-        
-    }
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
-        ArrayList<String> a1 = new ArrayList<>();
-        ArrayList<String> a2 = new ArrayList<>();
-        
-        for(int i = 0; i < arr1.length; i++){
-            a1.add(Change(arr1[i], n));
-            a2.add(Change(arr2[i], n));
+        for(int i = 0; i < n ; i++){
+            String temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+            temp = temp.substring(temp.length() - n);
+            temp = temp.replaceAll("1","#");
+            temp = temp.replaceAll("0"," ");
+            answer[i] = temp;
         }
-        
-        for(int i = 0; i < arr1.length; i++){
-            answer[i] = decode(a1.get(i), a2.get(i), n);
-        }
-        
         return answer;
     }
 }
