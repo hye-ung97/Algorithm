9efd432f27ad25e2477a0e74e8660ebd1a3eea9d
hye-ung97 <117243197+hye-ung97@users.noreply.github.com>
2023-07-29 15:10:49 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.55 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static char[] ch;
+    public int solution(String s) {
+
+        ch = s.toCharArray();
+
+        ch = s.toCharArray();
+        
+        for (int len = s.length(); len > 1; len--) {
+
+            for (int start = 0; start + len <= s.length(); start++) {
+                if(isPalindrome(start, len)){
+                    return len;
+                }
+            }
+        }
+
+        return 1;
+    }
+
+    public static boolean isPalindrome(int start, int len){
+
+        for (int i = 0; i < len /2; i++) {
+            if (ch[start + i] != ch[start + len - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
