31472b47739cc61a6da79c61acb17791edccbf64
혜영 <117243197+hye-ung97@users.noreply.github.com>
2023-11-09 08:52:38 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    var answer = 0;
+    if(a % 2 === 1 && b % 2 === 1){
+        return a ** 2 + b ** 2;
+    }
+    else if(a % 2 === 1 || b % 2 === 1){
+        return 2 * (a + b);
+    }
+    else {
+        return Math.abs(a - b);
+    }
+}
