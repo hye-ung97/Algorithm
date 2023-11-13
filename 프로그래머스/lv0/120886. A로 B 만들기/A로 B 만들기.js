6a01c3106704f7b1f8f32b0f3d5035eb5a44f39e
hye-young <hysung714@gmail.com>
2023-11-13 09:00:08 +0900

    refactoring : dir 위치 변경

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,5 +1,4 @@
 function solution(before, after) {
-    var answer = 0;
     let beforSort = [...before].sort().join('');
     let afterSort = [...after].sort().join('');
     return beforSort === afterSort ? 1 : 0;
