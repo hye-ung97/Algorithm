a061cee3ee2c110bbd50ae900762b06c2092565c
혜영 <117243197+hye-ung97@users.noreply.github.com>
2024-06-24 22:38:54 +0900

    [level 0] Title: 짝수는 싫어요, Time: 10.17 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.rangeClosed(1, n).filter(i -> i % 2 != 0).toArray();
+    }
+}
